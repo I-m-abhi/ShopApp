b25debf943dbd837837466e4efd5605bb19810e4
abhishek <abhisasa2001@gmail.com>
2022-09-12 16:45:38 +0530

    shopapp with redux

---
+++ src/containers/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>ShopApp</h2>
+      </div>
+      <div className="right">
+        <h3>Abhishek Kumar</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
